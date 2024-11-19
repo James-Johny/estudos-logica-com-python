92347c97e4acd86390d728ef034dfc241e34505c
James Johny <jamesjohny.sg@gmail.com>
2024-11-19 14:50:11 +0000

    Criação da lista

--- novos-codigos/ex01.py
+++ novos-codigos/ex01.py
@@ -1,1 +1,8 @@
 # Neste algoritmo, crie uma variável que armazene uma string e uma lista que armazena várias strings.
+
+nome = 'John'
+lista_de_compras = ['Café', 'Açucar', 'Coador', 'Cafeteira']
+
+print(f"A lista de compras do {nome} é:")
+for i, item in enumerate(lista_de_compras, start=1):
+    print(f" {i}.{item}")
